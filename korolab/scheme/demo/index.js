9c875517cf8b659ab955fe472c5a632048e79578
Evgeny <gazdovsky@gmail.com>
2016-10-25 02:53:45 +0300

    optimize ?

--- korolab/scheme/demo/index.js
+++ korolab/scheme/demo/index.js
@@ -1,6 +1,12 @@
 $(function () {
 
-
+    $('a')
+        .mouseenter(function(){
+            $('#color').css('opacity', 1)
+        })
+        .mouseout(function(){
+            $('#color').css('opacity', 0)
+        })
 
 
 });
